b233b74b8122eec66b45e35f28117eb2606201f9
alidehghan11 <77375038+alidehghan11@users.noreply.github.com>
2021-06-19 08:22:46 -0400

    Add files via upload

---
+++ mt.sh
@@ -0,0 +1,23 @@
+
+
+sudo dpkg --add-architecture i386
+
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+
+sudo gpg -o /etc/apt/trusted.gpg.d/winehq.key.gpg --dearmor winehq.key
+
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+
+sudo apt update
+
+sudo apt install --install-recommends winehq-stable
+
+wine --version
+
+
+wget https://download.mql5.com/cdn/web/alpari/mt4/alpari4setup.exe -P /root/Desktop/
+
+wget https://selective-alphabets.000webhostapp.com/file/default.tpl -P /root/Desktop/
+
+wget https://selective-alphabets.000webhostapp.com/file/GoldHedgeEA.ex4 -P /root/Desktop/
+
