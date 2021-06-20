4ea42f6582ccba7d60a70de7fc9ee394dd1ed3c3
alidehghan11 <77375038+alidehghan11@users.noreply.github.com>
2021-06-20 04:41:07 +0000

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,4 +1,4 @@
-sudo apt-get upgrade
+# sudo apt-get upgrade
 
 sudo apt-get update
 
@@ -10,7 +10,9 @@ sudo apt update -y
 #installing screen
 sudo apt-get install screen -y
 
-
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+google-chrome
 
 docker run -p 6071:80 dorowu/ubuntu-desktop-lxde-vnc
 
