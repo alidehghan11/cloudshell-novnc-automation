a4d02929ac150e7197f65d97a7c3afad472326e6
alidehghan11 <77375038+alidehghan11@users.noreply.github.com>
2021-07-19 20:12:15 +0000

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -7,5 +7,5 @@ sudo ufw allow 3389/tcp
 sudo /etc/init.d/xrdp restart
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
-./ngrok authtoken [{1rRcPqG5lA0Aw8rnazn8b1X5l6g_3dFJDzrZdrM3hQY7kgfoe}]
+./ngrok authtoken [{"1rRcPqG5lA0Aw8rnazn8b1X5l6g_3dFJDzrZdrM3hQY7kgfoe"}]
 ./ngrok tcp 3389
